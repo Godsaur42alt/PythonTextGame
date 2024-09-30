02ea91a114f2c552ea22715fc2065e926ec88e7d
Godsaur42alt <143028713+Godsaur42alt@users.noreply.github.com>
2024-09-30 07:21:49 -0500

    Update character.py

--- PythonGame/character.py
+++ PythonGame/character.py
@@ -1,9 +1,18 @@
 from random import *
+
+def poison(target):
+      x=target.get_effects()
+      if x.get("poison")!=None and x.get("poison")[0]<=0:
+          target.set_mod(target.get_minmod())-(x.get("poison")[1])
+          x["poison"][0]-=1
+      target.set_effects(x)
+
 class Character:
   def __init__(self):
       self.health=100
       self.damage=10
       self.defence=5
+      self.effects={}
       self.attackModmin=-2
       self.attackModmax=2
       self.basehealth=self.health
@@ -38,24 +47,35 @@ class Character:
       return self.defence
   def set_defence(self, defence):
       self.defence=defence
+  def get_effects(self):
+      return self.effects
+  def set_effects(self,effects):
+      self.effects=effects
+  def add_effects(self,effect,dur=5,lev=1):
+      effectmod=[dur,lev]
+      self.effects.update({effect:effectmod})
+  def get_minmod(self):
+      return attackModmin
+  def get_maxmod(self):
+      return attackModmax
+  def set_mod(self,Modmin="null",Modmax="null"):
+      if Modmin=="null":
+          Modmin=self.attackModmin
+      if Modmax=="null":
+          Modmax=self.attackModmax
+      self.attackModmin=Modmin
+      self.attackModmax=Modmax
+  
 
 
-
-
-
-
-
-
-
-
-
-
+        
 class Player(Character):
   def __init__(self):
       super().__init__()
       self.name=""
-      self.inventory=[]
+      self.magics=[]
       self.kills=0#You murderer
+      self.powers={}
   def set_name(self, name):
       self.name=name
   def get_name(self):
@@ -63,8 +83,13 @@ class Player(Character):
   def get_kills(self):
       return self.kills
   def attack(self, target):
-      atd = (self.damage+randint(-2,2)) - target.get_defence()
+      atd = (self.damage+randint(self.attackModmin,self.attackModmax)) - target.get_defence()
       if atd > 0:
+          for p in self.powers:
+              if p=="poison":
+                  dur=self.powers[p]*4
+                  lev=self.powers[p]*3
+                  target.add_effects(p,dur,lev)
           target.set_health(target.get_health() - atd)
           print("You did " + str(atd))
       else:
@@ -75,8 +100,12 @@ class Player(Character):
               print("You missed")
       if target.get_health()<=0:
           self.kills+=1
-
-
+  def add_powers(self,power,level):
+      self.powers[power]=level
+  def get_powers(self):
+      return self.powers
+  def set_powers(self,powers):
+      self.powers=powers
 
 
 class Enemy(Character):
@@ -88,6 +117,7 @@ class Enemy(Character):
        return self.type
   def get_nature(self):
        return self.nature
+
 class Goblin(Enemy):
    def __init__(self):
        super().__init__()
@@ -101,6 +131,7 @@ class Goblin(Enemy):
        self.attackModmin = -3
        self.attackModmax = 1
        self.nature="craven"
+
 class Orc(Enemy):
    def __init__(self):
       super().__init__()
