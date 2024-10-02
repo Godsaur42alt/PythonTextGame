1805b45b88354020231fe55c30a535e05c8ca3fe
Godsaur42alt <143028713+Godsaur42alt@users.noreply.github.com>
2024-10-02 10:29:18 -0500

    Update Main.py

--- PythonGame/Main.py
+++ PythonGame/Main.py
@@ -6,7 +6,7 @@ player = Player()
 playinput = ""
 enemyArr = []
 
-
+print(len(enemyArr))
 
 
 def enemyActions(taker, pos=0, target=player):
@@ -43,11 +43,13 @@ def createEnemy(type="null"):
 
 
 
-def playerinput(text="", cap=False):
+def playerinput(text="", cap=False,strip=True):
    global playinput
    playinput = input(text)
    if cap == False:
        playinput = playinput.lower()
+   if strip== True:
+      playinput.strip()
    return (playinput)
 
 
@@ -56,6 +58,7 @@ def playerinput(text="", cap=False):
 def spawnEnemies(num1=1, num2=10, clear=True):
    global enemyArr
    if clear == True:
+       print("cl")
        enemyArr = []
    numE = randint(num1, num2)
    for r in range(numE):
@@ -68,45 +71,56 @@ def combat():
    global enemyArr
    turnCount = 1
    while len(enemyArr) > 0:
-       if playinput == "runaway":
+       if playinput == "r":
+           print("you ran away")
            break
        print("Turn " + str(turnCount))
        print("Your health is "+str(player.get_health()))
        for l in range(len(enemyArr)):
-           if enemyArr[l].get_health() <= 0:
+           print(l)
+           if enemyArr[l].get_health() <= 0 and len(enemyArr)!=0:
                print(enemyArr[l].get_type() + " has died")
                enemyArr.pop(l)
-           print(str(l) + ". You see a " + enemyArr[l].get_type() + " it has " + str(
-               enemyArr[l].get_health()) + " health")
+               if len(enemyArr)==0:
+                   print("You killed them All")
+                   break
+               print(l)
+           print(str(l+1) + ". Enemy: " + enemyArr[l].get_type() + "(" + str(enemyArr[l].get_health()) + "hp)")
        player.set_defence(player.get_base_defence())
+       if len(enemyArr) == 0:
+           print(k)
+           break
        while True:
            playerinput(
-               "what do you want to do \n attack \n defend \n runaway \n stats(looking at stats doesn't pass the turn)\n")
-           if playinput == "attack":
+               "what do you want to do \n A. attack        D. defend \n R. runaway       S. stats(keeps turn)\n")
+           if playinput == "a":
                playerinput("type in the number of the enemy you want to attack\n")
-               player.attack(enemyArr[int(playinput)])
+               while int(playinput)>len(enemyArr):
+                   print("That wasn't an enemy")
+                   playerinput()
+               player.attack(enemyArr[int(playinput)-1])
                break
-           elif playinput == "defend":
+           elif playinput == "d":
                player.set_defence(player.get_base_defence() + 5)
                print("you've increased your defence by 5")
                break
-           elif playinput == "runaway":
+           elif playinput == "r":
                break
-           elif playinput == "stat":
+           elif playinput == "s":
                playerinput(
-                   "Type my stats for your stats, type type to get the stats for a type of enemy, or type the number for the enemy you want")
-               if playinput == "my stats":
+                   "Type m for your stats, type i to get the enemy index, or type the number of the enemy you want info about\n")
+               if playinput == "m":
                    print("Your name is " + player.get_name() + "\n  your health is " + str(
                        player.get_health()) + "\n  your damage is " + str(
                        player.get_damage()) + "\n  your defence is " + str(player.get_defence()))
-               elif playinput == "type":
+               elif playinput == "i":
                    playerinput("Type the type of enemy you want to know about\n")
                    if playinput == "goblin":
                        r = Goblin()
                    elif playinput == "orc":
                        r = Orc()
                    print("a "+r.get_type()+"'s health is " + str(r.get_health()) + "\n a"+r.get_type()+"'s damage is " + str(r.get_damage()) + "\n a "+r.get_type()+"'s defence is " + str(r.get_defence()))
-       for x in range(len(enemyArr)):
+       for x in range(len(enemyArr)-1):
            enemyActions(enemyArr[x],x)
        turnCount += 1
 
@@ -124,7 +138,7 @@ def combat():
 # print(player.get_health())
 # player.attack(goblin)
 # print(player.get_kills())
-spawnEnemies(1,3)
-print(enemyArr[0].get_base_health())
-combat()
-
+while playinput!="quit":
+    spawnEnemies(1,3)
+    print(enemyArr)
+    combat()
